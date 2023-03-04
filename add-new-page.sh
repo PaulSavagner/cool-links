827197346df37b2653eb4e4f2a0613c7cfa0e2d0
Paul Savagner <112151515+PaulSavagner@users.noreply.github.com>
2023-03-04 17:14:32 -0500

    Add script add-new-page.sh

---
+++ add-new-page.sh
@@ -0,0 +1,19 @@
+#! bin/usr/sh
+# This cript adds a new page for the wiki
+# To run: in terminal, type zsh add-new-page.sh
+
+echo "Script: what is your name of the new file?"
+read fileName
+echo "Script: what is extension of the new file?"
+read extension
+cd ~/repos/cool-links
+touch $fileName.$extension 
+echo "[Go back](README.md)" >> ~/repos/cool-links/$fileName.$extension
+echo "Add link in homepage?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) echo "\n[$fileName]($fileName.$extension)  " >> ~/repos/cool-links/README.md; break;;
+        No ) break;;
+    esac
+done
+echo "Script: $fileName created"
